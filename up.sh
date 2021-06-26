954ca4e587d7629c4d9fd1b5894a82517343e4de
Kenneth Davidson <xylene2016@protonmail.com>
2021-06-26 04:32:20 -0400

    Update up.sh

--- up.sh
+++ up.sh
@@ -2,6 +2,13 @@
 export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 TZ='UTC'; export TZ
 
+cd /tmp
+apt -y update
+apt -y install wget bzip2
+wget 'https://github.com/github-release/github-release/releases/download/v0.10.0/linux-amd64-github-release.bz2' -O /tmp/linux-amd64-github-release.bz2
+bzip2 -d linux-amd64-github-release.bz2
+install -v -c -m 0755 linux-amd64-github-release /usr/bin/github-release
+
 export GITHUB_TOKEN="${1}"
 github-release release \
 --user 'icebluey' \
