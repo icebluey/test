3eab7fcf2fa2f8846912f431aee16e962074e95a
Kenneth Davidson <xylene2016@protonmail.com>
2022-01-29 11:33:40 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+
+
+_setup_env() {
+    set -e
+    _tmp_dir="$(mktemp -d)"
+    cd "${_tmp_dir}"
+    wget -c -t 9 -T 9 "https://raw.githubusercontent.com/icebluey/setup-env/master/setup-env.sh"
+    bash setup-env.sh
+    cd /tmp
+    rm -fr "${_tmp_dir}"
+    /sbin/ldconfig
+}
+
+yum install -y deltarpm
+yum install -y bash wget ca-certificates
+ln -svf bash /bin/sh
+
+_setup_env
+
