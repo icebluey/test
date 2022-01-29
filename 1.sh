9f8d7028bffb5de704462c3aea9569355fe6bee3
Kenneth Davidson <xylene2016@protonmail.com>
2022-01-29 11:20:35 -0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
+
+systemctl daemon-reload > /dev/null 2>&1 || : 
+sleep 1
+systemctl stop docker.socket > /dev/null 2>&1 || : 
+systemctl stop docker.service > /dev/null 2>&1 || : 
+sleep 1
+systemctl stop containerd.service > /dev/null 2>&1 || : 
+sleep 1
+ip link set docker0 down > /dev/null 2>&1 || : 
+sleep 1
+ip link delete docker0 > /dev/null 2>&1 || : 
+sleep 1
+rm -fr /run/containerd
+rm -fr /run/docker.sock
+rm -fr /var/run/containerd
+rm -fr /var/run/docker.sock
+rm -fr /var/lib/docker/*
+
+echo '{
+  "dns": [
+    "8.8.8.8"
+  ],
+  "exec-opts": [
+    "native.cgroupdriver=systemd"
+  ],
+  "storage-driver": "overlay2"
+}' > /etc/docker/daemon.json
+rm -f /etc/docker/key.json
+sleep 1
+systemctl start docker.service > /dev/null 2>&1 || : 
+
 cat /etc/os-release
 echo
 systemctl list-unit-files | grep -i enabled
@@ -12,5 +43,7 @@ echo
 docker info
 echo
 docker images
+echo
+ip addr
 echo
 exit
