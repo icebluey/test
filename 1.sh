5d05edbc6219209354f393fcb293c9dc79e8c739
Kenneth Davidson <xylene2016@protonmail.com>
2022-01-29 11:06:16 -0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,9 +2,14 @@
 apt update -y
 apt upgrade -fy
 cat /etc/os-release
-systemctl list-unit-files
+echo
+systemctl list-unit-files | grep -i enabled
+echo
 systemctl status -l docker
 docker --version
 ls -la /etc/docker/
+echo
 cat /etc/docker/daemon.json
+echo
+docker info
 exit
