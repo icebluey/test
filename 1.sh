39a378ea4c17ae88c69dc216c4084faaeb3625f8
Kenneth Davidson <xylene2016@protonmail.com>
2024-09-15 05:59:39 -0400

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,17 +1,19 @@
 apt update -y -qqq
 #apt upgrade -fy
 
-systemctl list-unit-files | grep -i -E 'docker|container|pod'
+/bin/systemctl disable $(/bin/systemctl list-unit-files | grep -i -E 'docker|container|podman' | grep -iv 'container-getty' | awk '{print $1}' | sort -V | uniq | paste -sd" ")
+/bin/systemctl stop $(/bin/systemctl list-unit-files | grep -i -E 'docker|container|podman' | grep -iv 'container-getty' | awk '{print $1}' | sort -V | uniq | paste -sd" ")
 
 # delete firefox
 apt autoremove --purge -y $(dpkg -l | grep -i -E 'firefox|firebird' | awk '{print $2}' | sort -V | uniq | paste -sd" ")
 
 # delete microsoft
-apt autoremove --purge -y $(dpkg -l | grep -i microsoft | awk '{print $2}' | sort -V | uniq | paste -sd" ")
+apt autoremove --purge -y $(dpkg -l | grep -i -E 'microsoft|libmono|mono-|monodoc' | awk '{print $2}' | sort -V | uniq | paste -sd" ")
 
 # delete docker
 apt autoremove --purge -y $(dpkg -l | grep -i -E 'docker|container' | awk '{print $2}' | sort -V | uniq | paste -sd" ")
-rm -fr /etc/docker /etc/containerd /etc/containerd /var/lib/docker*
+/bin/rm -fr /etc/docker /usr/libexec/docker /etc/containerd /var/lib/containerd /var/lib/docker*
+
 
 echo
 echo
