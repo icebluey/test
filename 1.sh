3e59d9195540a18b3a13a7020a934d90ea67af5a
Kenneth Davidson <xylene2016@protonmail.com>
2024-09-15 05:27:28 -0400

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,7 +1,9 @@
 apt update -y
-apt upgrade -fy
+#apt upgrade -fy
 cat /etc/os-release
-dpkg -l > /tmp/p.txt
+#dpkg -l > /tmp/p.txt
+# delete dotnet
+apt autoremove --purge -y $(dpkg -l | grep -i dotnet | awk '{print $2}' | paste -sd" ")
 echo
 echo
 cat /tmp/p.txt
